769b2405f2fea90624e8931dbce7149d4754e928
e-aravinthkumar <89761301+e-aravinthkumar@users.noreply.github.com>
2022-01-26 07:49:52 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -30,5 +30,20 @@ let menuButton = document.querySelector('#menu-bar');
     topBar.classList.remove('hidden');
     console.log(1);
   });
-
-  
+  //Swiper
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
